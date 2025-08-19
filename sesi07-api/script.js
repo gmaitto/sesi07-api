48a726bbe31c91cf8e083142faf1ddcfff814eb5
gmaitto <coutogui2007@gmail.com>
2025-08-19 09:44:13 -0300

    Add files via upload

---
+++ sesi07-api/script.js
@@ -0,0 +1,15 @@
+const pageInput = document.getElementById("pageInput")
+const searchBtn = document.getElementById("searchBtn")
+const resultsDiv = document.getElementById("results")
+
+async function fetchCharacters(page){
+    resultsDiv.innerHTML = "<p>Carregando...</p>"
+
+    try {
+        const response = await fetch(`https://rickandmortyapi.com/api/character`)
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        
+    }
+}
